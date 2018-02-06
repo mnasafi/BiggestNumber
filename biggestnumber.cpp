531814b57932ab3f3e6ba6da5fda1af8f8cce263
mnasafi <mnasafi@mail.csuchico.edu>
2018-02-05 18:07:04 -0800

    Variables were declared; stores input as variable

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Mustafa Nasafi
 #include<iostream>
 
 using namespace std; //so we don't need the prefix std::
@@ -6,12 +6,29 @@ using namespace std; //so we don't need the prefix std::
 int main()
 {
   
-  cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
+  //declare variables
+  int number1;
+  int number2;
 
+
+  cout << "Please enter a whole number:\n";
+  cin >> number1;
+
+  
+  cout << "Please enter another whole number:\n";
+  cin >> number2; 
+
+    
+  //if statement
+
+  
   cout<<"Of those two numbers, the biggest is: ";
+  
+  
+  
   cout<<endl<<"Thank you for playing.\n";
 
   return 0;
 }
+
+
