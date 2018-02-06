4c17eac4f1f37d8a501fee25f1502ade913bd89d
mnasafi <mnasafi@mail.csuchico.edu>
2018-02-05 19:01:13 -0800

    If else statement was added. Prints out the biggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   int number1;
   int number2;
+  int biggest;
 
 
   cout << "Please enter a whole number:\n";
@@ -20,13 +21,25 @@ int main()
 
     
   //if statement
+  
+  if(number1>number2) // checks to see if number1 > number2
+  {
+    biggest = number1; // assigns number1 value to biggest if number1 > number2
+
+  }
+
+  else
+  {
+    biggest = number2; // assings number2 to biggest if above condition wasn't true.
+
+  }
 
   
-  cout<<"Of those two numbers, the biggest is: ";
+  cout << "Of those two numbers, the biggest is: " << biggest;
   
   
   
-  cout<<endl<<"Thank you for playing.\n";
+  cout << endl << "Thank you for playing.\n";
 
   return 0;
 }
